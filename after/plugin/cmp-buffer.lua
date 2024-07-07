5783e0e68693d90b93ee2bf39abf07af2281d878
Anton Östman <anton.ostman@niradynamics.se>
2024-07-07 12:21:35 +0200

    Add work conf

---
+++ after/plugin/cmp-buffer.lua
@@ -0,0 +1,14 @@
+cmp = require('cmp')
+
+cmp.setup {
+  sources = {
+    {
+      name = 'buffer',
+      option = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end
+      }
+    }
+  }
+}
--- lua/conf/remap.lua
+++ lua/conf/remap.lua
@@ -22,6 +22,9 @@ vim.keymap.set('n', '<leader>wj', '<C-w><C-j>')
 vim.keymap.set('n', '<leader>wd', '<C-w>c')
 vim.keymap.set('n', '<leader>bd', function() buffer_delete() end)
 vim.keymap.set('n', '<leader>wj', '<C-w><C-j>')
+vim.keymap.set('n', '<leader><leader>', '<C-^>')
+vim.keymap.set('n', '<leader>;', 'gcc', {remap = true})
+vim.keymap.set('v', '<leader>;', 'gc', {remap = true})
 
 -- Tmux navigation
 
@@ -29,18 +32,4 @@ vim.keymap.set('n', '<C-h>', ':silent <C-U>TmuxNavigateRight<cr>')
 vim.keymap.set('n', '<C-l>', ':silent <C-U>TmuxNavigateLeft<cr>')
 vim.keymap.set('n', '<C-j>', ':silent <C-U>TmuxNavigateDown<cr>')
 vim.keymap.set('n', '<C-k>', ':silent <C-U>TmuxNavigateUp<cr>')
-
--- nvim terminal 
-vim.api.nvim_set_keymap("t", '<C-;>', "<C-\\><C-n>", {noremap = true})
-vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-
-
--- Projects
---vim.keymap.set('n', '<leader>pp', function() require'telescope'.extensions.projects.projects{} end)
--- v':Telescope neovim-project discover<enter>'':Telescope neovim-project discover<enter>'im.keymap.set('n', '<leader>Ph', ':Telescope neovim-project history<enter>', opts)
 
