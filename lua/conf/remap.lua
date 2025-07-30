d332384e4aa4850d516120b155d580e0f193e933
AntonOstman <antos931@student.liu.se>
2025-07-30 17:19:54 +0200

    Add header switching keybind

--- lua/conf/remap.lua
+++ lua/conf/remap.lua
@@ -20,6 +20,8 @@ local function buffer_delete()
         vim.cmd("bd")
     end
 end
+
+-- Random remaps
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>fs", vim.cmd.w)
 vim.keymap.set("n", "<leader>wq", vim.cmd.q)
@@ -37,6 +39,7 @@ vim.keymap.set('n', '<leader><leader>', '<C-^>')
 vim.keymap.set('n', '<leader>;', 'gcc', {remap = true})
 vim.keymap.set('v', '<leader>;', 'gc', {remap = true})
 
+-- add semicolon at end of line
 vim.keymap.set('n', '<leader>w;', 'm`A;<ESC>``')
 vim.keymap.set('n', '<leader>t', 'gt')
 
@@ -44,6 +47,75 @@ vim.keymap.set('n', '<leader>o', 'm`o<ESC>``')
 vim.keymap.set('n', '<leader>O', 'm`O<ESC>``')
 vim.keymap.set('n', '<leader>BD', ':bufdo bd<CR>') -- kill all buffers
 
+-- Show diagnostic error
+vim.keymap.set('n', '<leader>ds', vim.diagnostic.open_float) -- Open error floating
+
+function ToggleVirtualText()
+  local current = vim.diagnostic.config().virtual_text
+  local new_value = not current
+  vim.diagnostic.config({ virtual_text = new_value })
+  -- print("virtual_text is now: " .. tostring(new_value))
+end
+vim.api.nvim_set_keymap('n', '<leader>di', ':lua ToggleVirtualText()<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>de', vim.diagnostic.setloclist) -- Open all errors
+
+-- vim.keymap.set('n', '<leader>di', function() vim.diagnostic.config({virtual_text=true}) end) -- Set inline errors
+
+
+-- Swap to .h or .hpp when in .c or .cpp and vice versa
+vim.keymap.set('n', '<leader>sh', function()
+  local filepath = vim.api.nvim_buf_get_name(0)
+  local filename = vim.fn.fnamemodify(filepath, ":t:r")
+  local dir = vim.fn.fnamemodify(filepath, ":p:h")
+  local ext = vim.fn.fnamemodify(filepath, ":e")
+  local cur_dir_name = vim.fn.fnamemodify(filepath, ":p:h:t")
+
+  local swap_exts = {
+    c = { "h" },
+    cpp = { "hpp", "h" },
+    cc = { "hh", "h" },
+    h = { "c", "cpp", "cc" },
+    hpp = { "cpp" },
+    hh = { "cc" },
+    frag = { "vert" },
+    vert = { "frag" }
+  }
+  
+  -- TODO: Lookup can be made more efficient by splitting 
+  -- inc and src search on the found extension
+  local search_dirs = {
+      cur_dir_name, "inc", "include", "Inc", "Include", "src", "Src", "Source", "source", 
+  }
+
+  local targets = swap_exts[ext]
+  if not targets then
+    vim.notify("Unsupported file type: " .. ext, vim.log.levels.WARN)
+    return
+  end
+
+  local DEBUG = false
+  local searches = ""
+
+  for _, search_dir in ipairs(search_dirs) do
+    local new_dir = vim.fn.fnamemodify(filepath, ":p:h:h")
+      for _, new_ext in ipairs(targets) do
+        local alt_path = new_dir .. "/" .. search_dir .. "/" .. filename .. "." .. new_ext
+        if vim.fn.filereadable(alt_path) == 1 then
+          vim.cmd("edit " .. alt_path)
+          return
+        end
+        if DEBUG then
+            searches = searches .. alt_path .. '\n'
+        end
+      end
+  end
+
+  if DEBUG then
+      vim.notify("No matching header/source file found in " .. searches, vim.log.levels.INFO)
+  else
+      vim.notify("No matching header/source file found", vim.log.levels.INFO)
+  end
+end, { desc = "Swap header/source file" })
 
 -- Fugitive binds
 vim.keymap.set('n', '<leader>gl', ':G log<CR>')
@@ -62,15 +134,12 @@ vim.keymap.set('n', '<C-j>', ':silent <C-U>TmuxNavigateDown<cr>')
 vim.keymap.set('n', '<C-k>', ':silent <C-U>TmuxNavigateUp<cr>')
 
 -- Remove annoying autoformat
-
 vim.cmd('autocmd BufEnter * set formatoptions-=cro')
 vim.cmd('autocmd BufEnter * setlocal formatoptions-=cro')
 
 -- Start highlight when searcing, stop on esc, highligt on *
-
 vim.cmd([[nnoremap <silent> * :let @/= '\<' . expand('<cword>') . '\>' <bar> set hls <cr>]])
 vim.keymap.set('n', '<ESC>', ':set nohls<cr>', {silent=true})
-
 vim.api.nvim_create_autocmd("CmdlineEnter", {
   pattern = { "/", "?" },
   command = "set hlsearch",
